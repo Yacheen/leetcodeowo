7b50963d52eef686a73de1f82f47a20df0d78222
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-07-04 01:30:16 -0600

    completed merge link lists question

--- src/linked_list/merge_two_sorted_linked_lists.rs
+++ src/linked_list/merge_two_sorted_linked_lists.rs
@@ -1,17 +1,52 @@
-//what i got so far
-pub fn merge_two_lists(mut list1: Option<Box<ListNode>>, mut list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-  // fix
-let mut dummy = Some(Box::new(ListNode::new(0)));
-  let mut tail = dummy;
-  while let (Some(list1_node), Some(list2_node)) = (list1.clone(), list2.clone()) {
-    if list1_node.val < list2_node.val {
-      tail.as_mut().unwrap().next = Some(list1_node.clone());
-      list1 = list1_node.next;
-    }
-    else {
-      tail.as_mut().unwrap().next = Some(list2_node.clone());
-      list2 = list2_node.next;
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
     }
   }
-  tail
+}
+pub fn merge_two_lists(mut list1: Option<Box<ListNode>>, mut list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    // fix
+    let mut dummy = Some(Box::new(ListNode::new(0)));
+    let mut tail = dummy.as_mut();
+
+    loop {
+      if let Some(list1_node) = list1.as_mut() {
+        if let Some(list2_node) = list2.as_mut() {
+          if list1_node.val < list2_node.val {
+            tail.as_mut().unwrap().next = Some(list1_node.clone()); 
+            list1 = list1.unwrap().next;
+          }
+          else {
+            tail.as_mut().unwrap().next = Some(list2_node.clone());
+            list2 = list2.unwrap().next;
+          }
+          tail = tail.unwrap().next.as_mut();
+        }
+        else {
+          break;
+        }
+      } 
+      else {
+        break;
+      }
+    }
+
+    // something ran out, add to list
+    if let Some(_list1_node) = list1.as_ref() {
+      tail.as_mut().unwrap().next = Some(list1.unwrap().clone());
+    }
+    else if let Some(_list2_node) = list2.as_ref() {
+      tail.as_mut().unwrap().next = Some(list2.unwrap().clone());
+    }
+    dummy.unwrap().next
 }
